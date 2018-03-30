ebc9300303a1ba368e17f4d2fe8b6c9834e9f960
Ananth Penghat <apenghat@wireless-10-104-238-22.umd.edu>
2018-03-29 22:19:51 -0400

    Add script for changing color of cube

---
+++ UI-HoloLens/Assets/ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	public GameObject myCube;
+
+	// Use this for initialization
+	void Start () {
+		myCube = GameObject.Find("MyCube"); // Get the cube GameObject
+	}
+
+	public void colorBlue() { // Set color of cube to blue
+		Renderer rend = myCube.GetComponent<Renderer>();
+		rend.material.color = Color.blue;
+	}
+
+	public void colorGreen() {
+		Renderer rend = myCube.GetComponent<Renderer>();
+		rend.material.color = Color.green;
+	}
+
+	public void colorRed() {
+		Renderer rend = myCube.GetComponent<Renderer>();
+		rend.material.color = Color.red;
+	}
+}
